2d6bf31d31fcffe44eb0c5db39fc1cbda4fcbed4
zakaria <zakariamounji44499@gmail.com>
2026-07-02 16:53:14 +0100

    add monitoring part

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,13 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS so your Next.js frontend can access this backend
   app.enableCors({
     origin: 'http://localhost:3000', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(8000, '0.0.0.0'); // Running on port 8000 as configured in docker-compose
+  await app.listen(8000, '0.0.0.0'); 
 }
 bootstrap();
+
